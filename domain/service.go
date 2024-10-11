72e1d6028f484123d630cc6ec84ce4bb42500a6e
Businge931 <jimmybusinge9@gmail.com>
2024-10-11 10:00:08 +0300

    feat: implement Service interface through service struct(attach Repository interface)

--- domain/service.go
+++ domain/service.go
@@ -17,3 +17,31 @@ type Repository interface {
 	FindAll() ([]*Product, error)
 	Delete(code string) error
 }
+
+type service struct {
+	productRepo Repository
+}
+
+func NewProductService(productRepo Repository) Service {
+	return &service{productRepo: productRepo}
+}
+
+func (s *service) Find(code string) (*Product, error) {
+	return s.productRepo.Find(code)
+}
+
+func (s *service) Store(product *Product) error {
+	return s.productRepo.Store(product)
+}
+
+func (s *service) Update(product *Product) error {
+	return s.productRepo.Update(product)
+}
+
+func (s *service) FindAll() ([]*Product, error) {
+	return s.productRepo.FindAll()
+}
+
+func (s *service) Delete(code string) error {
+	return s.productRepo.Delete(code)
+}
