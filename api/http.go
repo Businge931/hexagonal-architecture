4140d5c15c982b3d4c7cb688f8abac881e5b23b8
Businge931 <jimmybusinge9@gmail.com>
2024-10-11 10:49:42 +0300

    feat: implement http-handler interface and its methods

---
+++ api/http.go
@@ -0,0 +1,91 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/Businge931/hexagonal-architecture/domain"
+)
+
+type handler struct {
+	productService domain.Service
+}
+
+func NewHandler(productService domain.Service) ProductHandler {
+	return &handler{productService: productService}
+}
+
+func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	code := chi.URLParam(r, "code")
+
+	p, err := h.productService.Find(code)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
+	json.NewEncoder(w).Encode(&p)
+
+}
+
+func (h *handler) Post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	p := &domain.Product{}
+
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = h.productService.Store(p)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(&p)
+}
+
+func (h *handler) Put(w http.ResponseWriter, r *http.Request) {
+	p := &domain.Product{}
+	err := json.NewDecoder(r.Body).Decode(&p)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = h.productService.Update(p)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(&p)
+}
+
+func (h *handler) Delete(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	code := chi.URLParam(r, "code")
+
+	err := h.productService.Delete(code)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (h *handler) GetAll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	p, err := h.productService.FindAll()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.NewEncoder(w).Encode(&p)
+}
